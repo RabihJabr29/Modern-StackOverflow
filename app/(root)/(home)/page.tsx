ef707dbc051e7b3566e1e457f8ac12ccfd746d4a
Rabih Jabr <rabihjabrz2@gmail.com>
2024-01-03 00:36:02 +0200

    Adding Basic Routes (auth, home) for the app

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      Home
+    </div>
+  );
+};
+
+export default Home;
