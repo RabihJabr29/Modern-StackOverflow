0315afc64123c07fe30b025cb9329ace6bb873e6
Rabih Jabr <rabihjabrz2@gmail.com>
2024-01-15 20:41:28 +0200

    Added the Question form component

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,12 @@
+import Question from "@/components/forms/Question";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Question />
+    </div>
+  );
 };
 
 export default page;
