74615cce19e92f92bc041eb4175939d311d78bf7
Rabih Jabr <rabihjabrz2@gmail.com>
2024-02-01 23:15:52 +0200

    Adding loading effect to the Home page

---
+++ app/(root)/(home)/loading.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+
+const HomeLoading = () => {
+  return (
+    <section>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="small-medium primary-gradient min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+            <span className="text-light-900">Ask a Question</span>
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mb-12 mt-11 flex flex-wrap items-center justify-between gap-5">
+        <Skeleton className="h-14 flex-1" />
+        <div className="hidden max-md:block">
+          <Skeleton className="h-14 w-28" />
+        </div>
+      </div>
+
+      <div className="my-10 hidden flex-wrap gap-6 md:flex">
+        <Skeleton className="h-9 w-40" />
+        <Skeleton className="h-9 w-40" />
+        <Skeleton className="h-9 w-40" />
+      </div>
+
+      <div className="flex flex-col gap-6">
+        {[1, 2, 3, 4, 5].map((item) => (
+          <Skeleton key={item} className="h-48 w-full rounded-xl" />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default HomeLoading;
